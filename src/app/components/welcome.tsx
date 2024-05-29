62f5ba079b510f360bf286e4eac083bc35a974b5
cbazzy <151874965+cbazzy@users.noreply.github.com>
2024-05-29 22:56:45 +0100

    Fixed shadow artifact

---
+++ src/app/components/welcome.tsx
@@ -0,0 +1,11 @@
+export default function Welcome() {
+  return (
+    <>
+      <h1 className="text-4xl font-semibold desktop:text-8xl">
+        Callum Basnett
+      </h1>
+      <h2 className="text-2xl desktop:text-4xl">Junior Software Developer</h2>
+      <button> Learn more... </button>
+    </>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,19 @@
 "use client";
 import Image from "next/image";
 import Particle from "./components/particles";
+import Welcome from "./components/welcome";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center">
+    <main className="w-screen h-screen flex flex-col items-center justify-center">
       <Particle />
       <br></br>
+      <Welcome />
       <br></br>
       <br></br>
       <br></br>
       <br></br>
-      <h1 className="text-4xl font-semibold desktop:text-8xl">
-        Callum Basnett
-      </h1>
-      <h2 className="text-2xl desktop:text-4xl">Junior Software Developer</h2>
       <br></br>
-      <br></br>
-      <p>Test Page! </p>
       <br></br>
       <br></br>
       <br></br>
