795e60c266297a95a952d65e35f49c46f18583c1
cbazzy <151874965+cbazzy@users.noreply.github.com>
2024-05-30 04:18:48 +0100

    padding

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,11 +8,7 @@ export default function Home() {
   return (
     <>
       <NavBar />
-      <main className="w-screen h-screen flex flex-col px-[20%] desktop:px-[30%]">
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+      <main className="w-screen h-screen flex flex-col px-[20%] desktop:px-[30%] desktop:pt-40">
         <br></br>
         <p className="animate-fade-up animate-duration-800 animate-once text-lg font-semibold">
           Hello!
