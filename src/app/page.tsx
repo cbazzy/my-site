2bb41dea91eabedd5d53032044f7292aa90cb26f
cbazzy <151874965+cbazzy@users.noreply.github.com>
2024-05-30 04:25:32 +0100

    padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Welcome from "./components/welcome";
 export default function Home() {
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-center animate-fade">
-      <div className="desktop:pb-40 items-center flex flex-col">
+      <div className="pb-40 items-center flex flex-col">
         <Welcome />
       </div>
       <Particle />
