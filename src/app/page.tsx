910f77be399ee785abe869b8a94f57a0a46cdccb
cbazzy <151874965+cbazzy@users.noreply.github.com>
2024-06-17 14:46:57 +0100

    Scrolling on landing page disabled

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,19 @@
 import Image from "next/image";
 import Particle from "./components/particles";
 import Welcome from "./components/welcome";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    // Disable scrolling on mount
+    document.body.style.overflow = "hidden";
+
+    // Re-enable scrolling on unmount
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, []);
+
   return (
     <main className="overscroll-none overflow-hidden select-none w-screen h-screen flex flex-col items-center justify-center animate-fade caret-transparent">
       <div className="pb-40 items-center flex flex-col">
