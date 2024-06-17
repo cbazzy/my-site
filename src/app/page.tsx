40c2812cf8c755ee7a7f61f195d5143c6af69ea1
cbazzy <151874965+cbazzy@users.noreply.github.com>
2024-06-17 14:43:31 +0100

    test to disable scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Welcome from "./components/welcome";
 
 export default function Home() {
   return (
-    <main className="select-none w-screen h-screen flex flex-col items-center justify-center animate-fade caret-transparent">
+    <main className="overscroll-none overflow-hidden select-none w-screen h-screen flex flex-col items-center justify-center animate-fade caret-transparent">
       <div className="pb-40 items-center flex flex-col">
         <Welcome />
       </div>
